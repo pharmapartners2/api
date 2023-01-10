9cf148084bf65231a292656acdeed316254ec351
AmberCDH <kanarieamber@gmail.com>
2023-01-10 11:00:49 +0100

    Unused routes deleted

--- src/main/java/com/pharmapartners2/api/controller/AppointmentController.java
+++ src/main/java/com/pharmapartners2/api/controller/AppointmentController.java
@@ -1,21 +1,10 @@
 package com.pharmapartners2.api.controller;
 
 import com.pharmapartners2.api.dao.AppointmentModel;
-import com.pharmapartners2.api.dao.PatientModel;
-import com.pharmapartners2.api.model.AppointmentByDatum;
-import com.pharmapartners2.api.model.AppointmentRequest;
 import com.pharmapartners2.api.repository.AppointmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.HttpClientErrorException;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -33,16 +22,4 @@ public class AppointmentController {
     public AppointmentModel setAppointment(@RequestBody AppointmentModel appointmentModel){
         return appointmentRepository.save(appointmentModel);
     };
-
-    @GetMapping("/appointment/day")
-    public @ResponseBody List<AppointmentModel> getAppointmentByDate(@RequestBody AppointmentByDatum appointmentByDatum){
-        try {
-            //Date parsedDatum = DateFormat.getDateInstance().parse(appointmentByDatum.datum());
-            Date parsedDate = new SimpleDateFormat("yyyy/MM/dd").parse(appointmentByDatum.datum());
-            return appointmentRepository.findByDatum(parsedDate);
-        } catch (ParseException e) {
-            return null;
-        }
-
-    }
 }
--- src/main/java/com/pharmapartners2/api/repository/AppointmentRepository.java
+++ src/main/java/com/pharmapartners2/api/repository/AppointmentRepository.java
@@ -2,13 +2,7 @@ package com.pharmapartners2.api.repository;
 
 import com.pharmapartners2.api.dao.AppointmentModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.Date;
-import java.util.List;
 
 public interface AppointmentRepository extends JpaRepository<AppointmentModel, Integer> {
-    AppointmentModel findById(int patientId);
-    @Query(value = "select * from Afspraak where datum = CAST(CURRENT_TIMESTAMP AS DATE)", nativeQuery = true)
-    List<AppointmentModel> findByDatum(Date datum);
+    AppointmentModel findById(int appointmentId);
 }
